c9124f07295588552c0d68e6e5264bcab479a4c7
mario <mariosisneros22@gmail.com>
2026-02-03 17:05:28 -0500

    cambios en las pages y orden estrucutura de archivos

---
+++ src/assets/pages/index/indexImage.ts
@@ -0,0 +1,23 @@
+import img1 from './hero/hero_1.jpg';   
+import img2 from './hero/hero_2.jpg';
+import img3 from './hero/hero_3.jpg';
+
+import about from './about.jpg';
+
+export { about };
+export const heroImages = [img1, img2, img3];
+
+export const aboutSlides = [
+  {
+    title: 'Quiénes somos',
+    text: 'VALCER es una empresa especializada en servicios industriales y construcción, enfocada en calidad y seguridad.'
+  },
+  {
+    title: 'Experiencia',
+    text: 'Contamos con más de 15 años de experiencia desarrollando proyectos industriales y de infraestructura.'
+  },
+  {
+    title: 'Proyectos realizados',
+    text: 'Hemos ejecutado múltiples proyectos industriales y de construcción a nivel nacional.'
+  }
+];
