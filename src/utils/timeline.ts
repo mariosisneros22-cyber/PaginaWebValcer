3c8abdba61a645da344cad5245f45ac91b00e2eb
mario <mariosisneros22@gmail.com>
2026-02-09 18:29:09 -0500

    cambios en timelines

---
+++ src/utils/timeline.ts
@@ -0,0 +1,87 @@
+export interface TimelineItemData{
+  id: number;
+  date: string;
+  title: string;
+  description: string;
+
+}
+
+export const timelineData: TimelineItemData[] =[
+  {
+    id: 1,
+    date: '2007',
+    title: 'Fundación de la empresa',
+    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes'
+  },
+  {
+    id: 2,
+    date: '2010',
+    title: 'Primer gran proyecto',
+    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.'
+  },
+  {
+    id: 3,
+    date: '2023',
+    title: 'Expansión regional',
+    description:
+      'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+  },
+  {
+    id: 4,
+    date: '2007',
+    title: 'Fundación de la empresa',
+    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes'
+  },
+  {
+    id: 5,
+    date: '2010',
+    title: 'Primer gran proyecto',
+    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidadEjecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidadEjecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidadEjecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad'
+  },
+  {
+    id: 6,
+    date: '2023',
+    title: 'Expansión regional',
+    description:
+      'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento. Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+  },
+  {
+    id: 7,
+    date: '2007',
+    title: 'Fundación de la empresa',
+    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes'
+  },
+  {
+    id: 8,
+    date: '2010',
+    title: 'Primer gran proyecto',
+    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.'
+  },
+  {
+    id: 9,
+    date: '2023',
+    title: 'Expansión regional',
+    description:
+      'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+  },
+  {
+    id: 10,
+    date: '2007',
+    title: 'Fundación de la empresa',
+    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes'
+  },
+  {
+    id: 11,
+    date: '2010',
+    title: 'Primer gran proyecto',
+    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.'
+  },
+  {
+    id: 12,
+    date: '2023',
+    title: 'Expansión regional',
+    description:
+      'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+  },
+
+]
