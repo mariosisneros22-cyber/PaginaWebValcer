7b365bca4ab0431b0ae5d94dc3cc9472f8ed5f71
mario <mariosisneros22@gmail.com>
2026-02-10 13:22:03 -0500

    feat/avance timeline

--- src/utils/timeline.ts
+++ src/utils/timeline.ts
@@ -1,8 +1,15 @@
+import imagetest from '../assets/images/nosotros/nosotrosHero.jpg'
+
+
+
+
 export interface TimelineItemData{
   id: number;
   date: string;
   title: string;
   description: string;
+  image?: ImageMetadata;
+  imageAlt?: string;
 
 }
 
@@ -11,77 +18,98 @@ export const timelineData: TimelineItemData[] =[
     id: 1,
     date: '2007',
     title: 'Fundación de la empresa',
-    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes'
+    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
+
   },
   {
     id: 2,
     date: '2010',
     title: 'Primer gran proyecto',
-    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.'
+    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
   {
     id: 3,
     date: '2023',
     title: 'Expansión regional',
-    description:
-      'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+    description: 'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
   {
     id: 4,
     date: '2007',
     title: 'Fundación de la empresa',
-    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes'
+    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
   {
     id: 5,
     date: '2010',
     title: 'Primer gran proyecto',
-    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidadEjecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidadEjecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidadEjecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad'
+    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidadEjecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidadEjecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidadEjecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
   {
     id: 6,
     date: '2023',
     title: 'Expansión regional',
-    description:
-      'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento. Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+    description: 'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento. Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
   {
     id: 7,
     date: '2007',
     title: 'Fundación de la empresa',
-    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes'
+    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
   {
     id: 8,
     date: '2010',
     title: 'Primer gran proyecto',
-    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.'
+    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
   {
     id: 9,
     date: '2023',
     title: 'Expansión regional',
-    description:
-      'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+    description: 'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
   {
     id: 10,
     date: '2007',
     title: 'Fundación de la empresa',
-    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes'
+    description: 'Iniciamos con el objetivo de brnidar  soluciones industriales seguras y eficientes',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
   {
     id: 11,
     date: '2010',
     title: 'Primer gran proyecto',
-    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.'
+    description: 'Ejecutamos nuestro primer proyecto de gran escala cumpliendo altos estándares de calidad.',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
   {
     id: 12,
     date: '2023',
     title: 'Expansión regional',
-    description:
-      'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+    description: 'Ampliamos nuestras operaciones a nuevas regiones, consolidando nuestro crecimiento.',
+    image: imagetest,
+    imageAlt: 'Fundación de la empresa',
   },
 
 ]
