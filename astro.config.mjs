e7e93ebfff20e40f157195b0fa3c04f25a4aac6f
mario <mariosisneros22@gmail.com>
2026-03-02 10:28:36 -0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [react()],
 });
